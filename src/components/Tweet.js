c8ca68ea77306d1af7b6610e7482832048b3dbb3
glongh <glongh@gmail.com>
2018-08-15 21:45:43 -0400

    feat: New Tweet component and functionality

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
-// import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
-// import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
-// import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
-
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
 
-        //TODO: handle like tweet
+        const { dispatch, tweet, authedUser } = this.props
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     toParent = (e, id) => {
@@ -62,7 +65,9 @@ class Tweet extends Component {
     }
 }
 
-function mapStateToProps({ authedUser, users, tweets }, { id }) {
+function mapStateToProps(state, ownProps) {
+    const { authedUser, users, tweets } = state;
+    const { id } = ownProps;
     const tweet = tweets[id]
     const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
