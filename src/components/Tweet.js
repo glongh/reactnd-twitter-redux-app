deeb8ebafd838c0b3009a1049e0c0d209ddf466e
glongh <glongh@gmail.com>
2018-08-18 13:36:49 -0400

    feat: React-router added    - Router and routes    - Redirection to home in case of new tweet    - Nav menu with links to home and new tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
@@ -19,7 +20,7 @@ class Tweet extends Component {
 
     toParent = (e, id) => {
         e.preventDefault()
-        //TODO: Redirect to parent Tweet. 
+        this.props.history.push(`/tweet/${id}`)
     }
 
     render() {
@@ -30,11 +31,11 @@ class Tweet extends Component {
         }
 
         const {
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, replies, parent, id
         } = tweet
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src={avatar} alt={`Avatar of ${name}`}
                     className='avatar'
@@ -60,7 +61,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -79,4 +80,4 @@ function mapStateToProps(state, ownProps) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
