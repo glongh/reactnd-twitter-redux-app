f0abe7312634cd8c5a837cc34ef616d7d445eb09
glongh <glongh@gmail.com>
2018-08-18 12:08:02 -0400

    fest: Adding new tweet logic

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets';
 
 class NewTweet extends Component {
     state = {
@@ -15,12 +17,11 @@ class NewTweet extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-
+        
         const { text } = this.state
+        const { dispatch, id } = this.props
 
-        // todo: Add Tweet to Store
-
-        console.log('New Tweet')
+        dispatch(handleAddTweet({text, id}))
 
         this.setState(() => {
             text: ''
@@ -62,4 +63,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
